e63c467e2ba802402b3ba9cbb07487b80a8c0df7
MichalWes <massive7m@gmail.com>
2022-03-06 20:46:19 +0100

    Last task

---
+++ 2taskAll1.js
@@ -0,0 +1,13 @@
+const x = 50;
+
+const message1 = "X > 100";
+const message2 = "X is mediocre";
+const message3 = "X is small";
+
+if (x > 100) {
+  console.log(message1.toUpperCase());
+} else if (x > 30) {
+  console.log(message2.toUpperCase());
+} else {
+  console.log(message3.toUpperCase());
+}
