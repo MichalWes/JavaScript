3268597b1f76f81b7f643ccb3849fec12b8a7c22
MichalWes <massive7m@gmail.com>
2022-03-06 20:49:29 +0100

    fix

--- 2taskAll1.js
+++ 2taskAll1.js
@@ -4,7 +4,7 @@ const message1 = "X > 100";
 const message2 = "X is mediocre";
 const message3 = "X is small";
 
-if (x > 100) {
+if (x >= 100) {
   console.log(message1.toUpperCase());
 } else if (x > 30) {
   console.log(message2.toUpperCase());
