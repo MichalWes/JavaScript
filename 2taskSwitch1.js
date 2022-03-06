39d40f107a5440c539dd4748a101154a72714c32
MichalWes <massive7m@gmail.com>
2022-03-06 20:16:01 +0100

    Switch and ternary tasks

---
+++ 2taskSwitch1.js
@@ -0,0 +1,18 @@
+const promo = "20%";
+
+switch (promo) {
+  case "10%":
+    console.log("Today we have 10% discount");
+    break;
+
+  case "20%":
+    console.log("Today we have 20% discount");
+    break;
+
+  case "30%":
+    console.log("Today we have 30% discount");
+    break;
+
+  default:
+    console.log("Today we have a special offer");
+}
---
+++ 2taskTernary1.js
@@ -0,0 +1,2 @@
+const x = 10;
+console.log(x % 2 == 0 ? "X is even" : "X is odd");
