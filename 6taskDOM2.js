9359450867c999f5a74d7933baddc8264d424cef
MichalWes <massive7m@gmail.com>
2022-05-22 17:59:35 +0200

    6taskDOM2

--- 6taskDOM2.js
+++ 6taskDOM2.js
@@ -1,14 +1,15 @@
-const div = document.querySelector("div");
 const list = document.createElement("ul");
-div.append(list);
+document.body.append(list);
 
-// for (let i = 1; i < 10; i++) {
-// 	listElement = document.createElement("li");
-// 	listElement.innerHTML(i);
-// 	list.append(listElement);
-// }
-
-for (let i = 1; i < 10; i++) {
-	const listElement = document.createElement("li");
-	listElement.list.append(listElement);
+for (let i = 1; i <= 10; i++) {
+  const listElement = document.createElement("li");
+  listElement.textContent = i;
+  list.append(listElement);
 }
+
+const listElements = document.querySelectorAll("li");
+const last = listElements[listElements.length - 1];
+last.textContent = "Jestem ostatnim elementem z listy";
+last.style.padding = "20px 40px";
+last.style.fontSize = "48px";
+last.style.backgroundColor = "blue";
